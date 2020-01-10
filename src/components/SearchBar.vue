536d7eaeab459bb739a63f79dabad4c060f325f6
Eduard <eddmos86@gmail.com>
2020-01-10 17:39:12 +0300

    first commit

---
+++ src/components/SearchBar.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <input @input="onInput" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SearchBar',
+  methods: {
+    onInput: function() {
+      // console.log(event.target.value)
+    }
+  }
+}
+</script>
+
+<style >
+
+</style>
